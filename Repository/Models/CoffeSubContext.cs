fbdf411f8ad365c8f4510931f98419fb8b231d98
NhatMinh <mcpegunny@gmail.com>
2025-08-12 13:53:22 +0700

    add connection string

--- Repository/Models/CoffeSubContext.cs
+++ Repository/Models/CoffeSubContext.cs
@@ -35,9 +35,9 @@ public partial class CoffeSubContext : DbContext
 
     public virtual DbSet<UserSubscription> UserSubscriptions { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=MSI\\MSSQL2022EXPRESS;Uid=sa;Pwd=12345;Database=coffe_sub;TrustServerCertificate=True");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=MSI\\MSSQL2022EXPRESS;Uid=sa;Pwd=12345;Database=coffe_sub;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
